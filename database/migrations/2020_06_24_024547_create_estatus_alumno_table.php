f16f76f4feef1ae306815533969029ad5784d4e0
giovannihasid <61325719+giovannihasid@users.noreply.github.com>
2020-06-24 17:33:39 -0500

    gio

--- database/migrations/2020_06_24_024547_create_estatus_alumno_table.php
+++ database/migrations/2020_06_24_024547_create_estatus_alumno_table.php
@@ -14,7 +14,13 @@ class CreateEstatusAlumnoTable extends Migration
     public function up()
     {
         Schema::create('estatus_alumno', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id')->primary();
+            $table->uuid('id_alumno');
+            $table->uuid('id_movimiento_alumno');
+            $table->date('fecha');
+            $table->text('comentario')->nullable();            
+            $table->foreign('id_alumno')->references('id')->on('alumno');
+            $table->foreign('id_movimiento_alumno')->references('id')->on('movimiento_alumno');
             $table->timestamps();
         });
     }
